5775ca549d6af50f17d9910fe79bac44f3ffeb77
Alfredo <fredy_rios0123@outlook.com>
2022-08-17 20:37:31 -0700

    Did not finish, something is broken, and I can't figure it out

--- src/App.js
+++ src/App.js
@@ -2,19 +2,69 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import Main from './Main.js';
-import Footer from './Footer.js';
+import data from './data.json';
+import SelectedBeast from './SelectedBeast';
 
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      beast: {},
+      // description: '',
+      // imageURL: '',
+      // imgSize: '',
+      // title: '',
+    }
+  }
 
+  handleClick = () => {
+    this.setState({
+      description: this.state.description,
+      imgURL: this.state.imgURL,
+      imgSize: this.state.imgSize,
+      title: this.state.title,
+    })
+    console.log(this.state);
+  }
+
+  handleShowModal = (beast) => {
+    this.setState({
+      showModal: true,
+      beast: beast,
+    })
+    console.log(beast);
+  }
+
+  handleHideModal = () => {
+    this.setState({
+      showModal: false,
+    })
+  }
 
   render() {
     return (
       <>
         <Header/>
-        <Main/>
-        <Footer/>
+        <Main
+          data={data}
+          img={this.state.img}
+          handleClick={this.handleClick}
+          handleShowModal={this.handleShowModal}
+        />
+        <footer>
+        &copy; Alfredo Orquiz 2022
+        </footer>
+        <SelectedBeast
+          beast={this.state.beast}
+          handleShowModal={this.handleShowModal}
+          handleHideModal={this.handleHideModal}
+          show={this.state.showModal} 
+          onHide={this.handleHideModal}
+        ></SelectedBeast>
+
       </>
     );
   }
